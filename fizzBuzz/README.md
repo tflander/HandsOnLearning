42194a8af9c38886f020023ed90dd02a809a04fe
robertfmurdock <rmurdock@pillartechnology.com>
2014-11-26 17:34:22 -0500

    Changed the name of legacy rescue kata and fixed an invalid name on the Renewal kata.

--- fizzBuzz/README.md
+++ fizzBuzz/README.md
@@ -1,8 +1,13 @@
 Goal
+====
+
 For this project we would like you to design a program to recreate the FizzBuzz numbers game.
 
 Rules
+-----
+
 For the integers 1 through 100 inclusive program should:
+
 - return Fizz for any multiple of 3
 - return Buzz for any multiple of 5
 - return FizzBuzz for any multiple of 3 & 5
--- renewalMockKata/src/test/java/RenewalNotificationServiceTest.java
+++ renewalMockKata/src/test/java/RenewalNotificationServiceTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class RenewalNotificationServiceTest {
 
     @Test
-    public void shouldFailSometimesAndPassSometimesWhichIsAbsolutelyUseless() {
+    public void notifyingAtRiskSubscribersShouldSendEmails() {
         RenewalNotificationService renewalNotificationService = new RenewalNotificationService(new RealSubscriberService(), null);
         renewalNotificationService.notifyAtRiskSubscribers();
 //		Uh oh... how will we know what happened if the method doesn't return any information?
