786fff76a13e83bc24424069600c58aab4fc2a7d
Biju Rajan <brajan@Bijus-MacBook-Pro.local>
2017-09-27 10:29:43 -0400

    Adding Tennis Kata for csharp

---
+++ tennisScoring/csharp/TennisScoringTest/TennisGameEasyTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Katas
+{
+    public class TennisGameEasyTest : BaseTennisGameTest
+    {
+        
+        [Theory]
+        [ClassData(typeof(BaseTennisTestData))]
+        public void scoreIsCorrect(int p1Score, int p2Score, string expectedScore)
+        {
+            ITennisGame game = new TennisGameEasy("player1","player2");
+
+            scorePoints(game, "player1", p1Score);
+            scorePoints(game, "player2", p2Score);
+
+            string actualScore = game.GetScore();
+            Assert.Equal(expectedScore, actualScore);
+        }
+    }
+}
