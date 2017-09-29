b421d9bf0dabf3d55e68b0ec4da80d702f3b97b0
Biju Rajan <brajan@Bijus-MacBook-Pro.local>
2017-09-29 15:43:13 -0400

    Adding in dotnet framework code for FizzBuzz

---
+++ 01-FizzBuzz/csharp-dotnetframework/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Katas
+{
+    public class FizzBuzz
+    {
+		static void Main(string[] args)
+		{
+		}
+
+        public string Process(int inputValue){
+            return inputValue.ToString();
+        }
+    }
+}
---
+++ 01-FizzBuzz/csharp-dotnetframework/FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Katas
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void ShouldReturn1For1()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            string actualReturnValue = fizzBuzz.Process(1);
+            string expectedReturnValue = "1";
+            Assert.AreEqual(expectedReturnValue, actualReturnValue);
+        }
+    }
+}
