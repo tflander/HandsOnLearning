6e44bd6f9c31966e699abbc0117c95d99cb39f32
David Zuidema <dzuidema@pillartechnology.com>
2017-10-03 12:54:54 -0400

    Port Rock Paper Scissors to .NET Framework

---
+++ 02-RockPaperScissors/csharp-dotnetframework/RockPaperScissors/RockPaperScissors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Katas
+{
+    public class RockPaperScissors
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string determineWinner(string gesture1, string gesture2){
+            return null;
+        }
+    }
+}
---
+++ 02-RockPaperScissors/csharp-dotnetframework/RockPaperScissorsTest/RockPaperScissorsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Katas
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        [Ignore("Because")]
+        public void rockShouldCrushScissors()
+        {
+            RockPaperScissors game = new RockPaperScissors();
+
+            string actualResult = game.determineWinner("rock", "scissors");
+            string expectedResult = "rock beats scissors";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
