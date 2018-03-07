a3abb46a11eec2686cf81c8e88627d7318af8525
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-07 13:51:54 -0700

    Started adding unit tests that exist in java example

---
+++ 07-Banking-Kata/csharp-dotnetcore2/BankingKataAPI.Test/Models/AccountTest.cs
@@ -0,0 +1,25 @@
+﻿using BankingKataAPI.Models;
+using Xunit;
+
+namespace BankingKataAPI.Test.Models
+{
+    public class AccountTest
+    {
+        [Fact]
+        public void CanSpecifyAStartingBalance()
+        {
+            var account = new Account(new Money(65.98m));
+
+            Assert.Equal(account.Balance, new Money(65.98m));
+        }
+
+        [Fact]
+        public void ShouldGenerateIdForAccount()
+        {
+            var account1 = new Account(new Money());
+            var account2 = new Account(new Money());
+
+            Assert.NotEqual(account1.Id, account2.Id);
+        }
+    }
+}
