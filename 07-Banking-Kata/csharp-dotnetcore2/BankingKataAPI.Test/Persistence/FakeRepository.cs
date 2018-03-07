a2d6cbad843d013d07096bb859f28090e792a04f
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-07 14:05:50 -0700

    Continued to add tests to banking kata

---
+++ 07-Banking-Kata/csharp-dotnetcore2/BankingKataAPI.Test/Persistence/FakeRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankingKataAPI.Models;
+using BankingKataAPI.Persistence;
+
+namespace BankingKataAPI.Test.Persistence
+{
+    public class FakeRepository<T> : IRepository<T> where T : class, Identifiable
+    {
+        private readonly Dictionary<Guid, T> _items;
+
+        public FakeRepository()
+        {
+            _items = new Dictionary<Guid, T>();
+        }
+
+        public T FindOne(Guid id)
+        {
+            return _items[id];
+        }
+
+        public void Save(T item)
+        {
+            _items[item.GetId()] = item;
+        }
+    }
+}
---
+++ 07-Banking-Kata/csharp-dotnetcore2/BankingKataAPI.Test/Services/AccountsServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using BankingKataAPI.Models;
+using BankingKataAPI.Services;
+using BankingKataAPI.Test.Persistence;
+using Xunit;
+
+namespace BankingKataAPI.Test.Services
+{
+    public class AccountsServiceTest
+    {
+        private readonly FakeRepository<Account> _repository;
+        private readonly AccountsService _service;
+
+        public AccountsServiceTest()
+        {
+            _repository = new FakeRepository<Account>();
+            _service = new AccountsService(_repository);
+        }
+
+        [Fact]
+        public void OpeningAnAccountSavesItInTheAccountRepository()
+        {
+            var account = _service.OpenNewAccount(new Money());
+            Assert.NotNull(_repository.FindOne(account.GetId()));
+        }
+
+        [Fact]
+        public void NewAccountsStartWithCorrectBalance()
+        {
+            var expectedBalance = new Money(7.11m);
+
+            var account = _service.OpenNewAccount(expectedBalance);
+            var savedAccount = _repository.FindOne(account.GetId());
+
+            Assert.Equal(expectedBalance, savedAccount.Balance);
+        }
+
+        [Fact]
+        public void FindAccountFindsItFromTheRepository()
+        {
+            var expectedBalance= new Money(12.34m);
+            var account = new Account(expectedBalance);
+            _repository.Save(account);
+    
+            var savedAccount = _service.FindAccount(account.GetId());
+            Assert.Equal(expectedBalance, savedAccount.Balance);
+        }
+
+        [Fact]
+        public void FindAccountThrowsAnErrorIfAccountIsNotFound()
+        {
+            var nonExistantAccountId = Guid.NewGuid();
+
+            var exception = Assert.Throws<Exception>(() => _service.FindAccount(nonExistantAccountId));
+            Assert.Equal("Invalid account", exception.Message);
+        }
+    }
+}
