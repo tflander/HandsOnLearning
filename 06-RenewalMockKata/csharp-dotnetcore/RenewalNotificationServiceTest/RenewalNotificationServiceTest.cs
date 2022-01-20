41810fa26afd6eb788202bbab8aa00a2c48684e2
todd flanders <toddfbass@gmail.com>
2022-01-20 11:38:40 -0500

    updates from TDD learning session

--- 06-RenewalMockKata/csharp-dotnetcore/RenewalNotificationServiceTest/RenewalNotificationServiceTest.cs
+++ 06-RenewalMockKata/csharp-dotnetcore/RenewalNotificationServiceTest/RenewalNotificationServiceTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
 namespace Katas
@@ -6,11 +8,70 @@ namespace Katas
     public class RenewalNotificationServiceTest
     {
         [Fact]
-        public void notifyingAtRiskSubscribersShouldSendEmails()
+        public void NotifyingAtRiskSubscribersShouldSendEmails()
         {
-			RenewalNotificationService renewalNotificationService = new RenewalNotificationService(new RealSubscriberService(), null);
+	        var subscribersExpiringInTheNextThreeMonths = new List<string> { "a@b.com", "c@d.com" };
+	        var mockSubscriberService = new MockSubscriberService(subscribersExpiringInTheNextThreeMonths);
+	        var mockEmailService = new MockEmailService();
+	        
+			var renewalNotificationService = new RenewalNotificationService(
+				mockSubscriberService, 
+				mockEmailService);
+			
 			renewalNotificationService.notifyAtRiskSubscribers();
-			// Uh oh... how will we know what happened if the method doesn't return any information?
-		}
+			
+			mockEmailService.NumberOfCalls.Should().Be(1);
+			mockEmailService.LastEmailAddressesSent.Should().BeEquivalentTo(subscribersExpiringInTheNextThreeMonths);
+			mockEmailService.LastEmailMessage.Should().Be("Please renew your subscription to Ferret Fancy!");
+        }
+        
+        [Fact]
+        public void EmailServiceNotCalledWhenNoSubscribersAreExpiring()
+        {
+	        var mockSubscriberService = new MockSubscriberService(subscribersExpiringInTheNextThreeMonths: null);
+	        var mockEmailService = new MockEmailService();
+	        
+	        var renewalNotificationService = new RenewalNotificationService(
+		        mockSubscriberService, 
+		        mockEmailService);
+			
+	        renewalNotificationService.notifyAtRiskSubscribers();
+
+	        mockEmailService.NumberOfCalls.Should().Be(0);
+        }
+        
+        // He asks you to write a program that will email his subscribers that are 
+        // within three months of relapsing on their subscriptions the message "Please renew your subscription to Ferret Fancy!"
+        
+        //   Verify the date sent to the subscriber service is three months in the future.
+    }
+
+    public class MockEmailService : EmailService
+    {
+	    public void EmailMessage(string message, List<string> emails)
+	    {
+		    LastEmailAddressesSent = emails;
+		    LastEmailMessage = message;
+		    ++NumberOfCalls;
+	    }
+
+	    public List<string> LastEmailAddressesSent { get; private set; }
+	    public string LastEmailMessage { get; private set; }
+	    public int NumberOfCalls { get; private set; } = 0;
+    }
+
+    public class MockSubscriberService : SubscriberService
+    {
+	    private readonly List<string> _subscribersExpiringInTheNextThreeMonths;
+
+	    public MockSubscriberService(List<string> subscribersExpiringInTheNextThreeMonths)
+	    {
+		    _subscribersExpiringInTheNextThreeMonths = subscribersExpiringInTheNextThreeMonths;
+	    }
+
+	    public List<string> GetSubscribersThatWillExpireBetweenNowAndDate(int day, int month, int year)
+	    {
+		    return _subscribersExpiringInTheNextThreeMonths;
+	    }
     }
 }
